853cb7dbe6fbb7ff9462d3c265f7ec1ff13e6bb6
Shaul Gabrieli <shaulcasper@gmail.com>
2023-01-01 08:22:15 +0200

    fix img

--- views/home.jsx
+++ views/home.jsx
@@ -4,9 +4,9 @@ export function Home() {
   return (
     <section className="home flex justify-center align-center">    
 
-            <NavLink   className="home-links" to="/note"><img className="home-img" src="assets/img/note.png"></img></NavLink>
-            <NavLink   className="home-links" to="/mail"><img className="home-img" src="assets/img/mail.png"></img></NavLink>
-            <NavLink   className="home-links"  to="/note"><img className="home-img" src="assets/img/books.png"></img> </NavLink>
+            <NavLink   className="home-links" to="/note"><img className="home-img" src="/assets/img/note.png"></img></NavLink>
+            <NavLink   className="home-links" to="/mail"><img className="home-img" src="/assets/img/mail.png"></img></NavLink>
+            <NavLink   className="home-links"  to="/note"><img className="home-img" src="/assets/img/books.png"></img> </NavLink>
         
     </section>
   );
